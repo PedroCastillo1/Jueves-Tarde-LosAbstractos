8c864cc0759676be5f5c1195d8f25048cff7a156
Mora Camila Coghlan <moracoghlan@gmail.com>
2026-04-08 16:44:18 -0300

    Update Estrategia1MORA.java
    agregue el override a mis metodos

--- src/Estrategia1MORA.java
+++ src/Estrategia1MORA.java
@@ -6,33 +6,38 @@ public class Estrategia1MORA implements PilaTDA{
     // por ende para guardar la cantidad de cambios y llevar un contador, necesitamos una variable externa a la pila
 
     @Override
-    public void inicializarPila1(){
+    public void inicializarPila(){
         pila = new String[MAX];
         cantidad = 0;
     }
 
     @Override
-
-    public void apilar1 (String x){
+    public void apilar (String x){
         if (cantidad < MAX){
         pila[cantidad] = x;
         cantidad ++;
     }
     }
 
-    public void desapilar1(){
+    @Override
+    public void desapilar(){
         if (!pilaVacia()){
             cantidad --;
         }
     }
+    
 // al metodo tope le pusimos "reuperar" ya que cumpliria con la consigna mas literalmente, si quiero volver a un cambio anterior,
     // desapilo y despues recupero.
+
+    
     public String recuperar(){
         return pila[cantidad -1];
 
     }
 
-    public boolean pilaVacia1(){
+    
+    @Override
+    public boolean pilaVacia(){
         return cantidad == 0;
     }
 
