f6b1abe0a55f9db80d8f3acd250cade0de9428a1
Mora Camila Coghlan <moracoghlan@gmail.com>
2026-04-08 10:16:18 -0300

    Create Cambio.java
    agrego  mi clase Cambio

---
+++ src/Cambio.java
@@ -0,0 +1,11 @@
+public class Cambio {
+    private  String estado;
+
+    public String getEstado(){
+        return estado;
+    }
+    public Cambio (String estado){
+        this.estado = estado;
+    }
+
+}
