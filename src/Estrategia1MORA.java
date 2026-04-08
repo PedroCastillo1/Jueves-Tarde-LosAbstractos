47c8a3ade0258b15f1fc0d624fb7444baa98e55b
Mora Camila Coghlan <moracoghlan@gmail.com>
2026-04-08 10:18:06 -0300

    Create Estrategia1MORA.java
    agrego la clase de mi pila

---
+++ src/Estrategia1MORA.java
@@ -0,0 +1,40 @@
+public class Estrategia1MORA implements PilaTDA{
+    private String[] pila;
+    private static final int MAX = 100;
+    private int cantidad;
+// en este caso la estrategia mejor planteada es cantidad, ya que dentro de la pila queremos guadar estados, con strings.
+    // por ende para guardar la cantidad de cambios y llevar un contador, necesitamos una variable externa a la pila
+
+    @Override
+    public void inicializarPila1(){
+        pila = new String[MAX];
+        cantidad = 0;
+    }
+
+    @Override
+
+    public void apilar1 (String x){
+        if (cantidad < MAX){
+        pila[cantidad] = x;
+        cantidad ++;
+    }
+    }
+
+    public void desapilar1(){
+        if (!pilaVacia()){
+            cantidad --;
+        }
+    }
+// al metodo tope le pusimos "reuperar" ya que cumpliria con la consigna mas literalmente, si quiero volver a un cambio anterior,
+    // desapilo y despues recupero.
+    public String recuperar(){
+        return pila[cantidad -1];
+
+    }
+
+    public boolean pilaVacia1(){
+        return cantidad == 0;
+    }
+
+
+}
