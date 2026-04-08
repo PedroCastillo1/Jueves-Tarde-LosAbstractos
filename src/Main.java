e0ccd8da52d704db2ada63169beb07fb8f9433fd
Mora Camila Coghlan <moracoghlan@gmail.com>
2026-04-08 17:52:48 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,30 @@ public class Main {
         System.out.println("*********** Ejercicio Editor de Codigo**********")
         Cambio c = new Cambio("escribiendo");
         String estado = c.getEstado();
-        Estrategia1 p = new Estrategia1();
-        p.inicializarPila();
-        p.apilar(estado);
+        Estrategia1 p = new Estrategia();
+        p.InicializarPila();
+        p.Apilar(estado);
 
         Cambio c1 = new Cambio("dos nuevas lineas");
         String e1 = c1.getEstado();
-        p.apilar(e1);
+        p.Apilar(e1);
 
         Cambio c2 = new Cambio("borrar ultima linea");
         String e2 = c2.getEstado();
-        p.apilar(e2);
+        p.Apilar(e2);
 
         Cambio c3 = new Cambio("agregar comentario");
         String e3 = c3.getEstado();
-        p.apilar(e3);
+        p.Apilar(e3);
 
         Cambio c4 = new Cambio("corregir variable");
         String e4 = c4.getEstado();
-        p.apilar(e4);
+        p.Apilar(e4);
 
         // aca ya tenemos varios cambios apilados. que pasa si el usuario se arrepiente de los ultimos cambios
 
-        p.desapilar(); // ignora "corregir variable"
-        p.desapilar(); // ignora "agregar comentario"
+        p.Desapilar(); // ignora "corregir variable"
+        p.Desapilar(); // ignora "agregar comentario"
 
         String recupero = p.recuperar();
 
