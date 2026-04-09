8aa0ff1031181b3067a020d26d3b4f7b62bd34b7
PedroCastillo1 <pedrocastillomarin22@gmail.com>
2026-04-09 14:06:32 -0300

    Moved tda dentro del proyecto

--- src/Interfaces/ColaPrioridadTDA.java
+++ src/Interfaces/ColaPrioridadTDA.java
@@ -2,7 +2,7 @@ package Interfaces;
 
 public interface ColaPrioridadTDA<T> {
     void InicializarColaPrioridad();
-    void AcolarPrioridad(T numero, int prioridad);
+    void AcolarPrioridad(T valor, int prioridad);
     void Desacolar();
     boolean ColaVacia();
     T Primero();
