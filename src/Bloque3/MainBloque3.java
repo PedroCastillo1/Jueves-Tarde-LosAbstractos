1dbc5a358ff43ba0d29320697036b524c4b89045
Mateo Marta <martamateotomas@gmail.com>
2026-04-09 21:22:40 -0300

    Implement priority queue in MainBloque3

--- src/Bloque3/MainBloque3.java
+++ src/Bloque3/MainBloque3.java
@@ -1,7 +1,28 @@
 package Bloque3;
+import Bloque3.Implementacion.GuardiaDeHospital;
+import Bloque3.Interface.ColaPrioridadTDA;
+
 
 public class MainBloque3 {
     public static void main(String[] args) {
+        ColaPrioridadTDA<Integer> cola = new GuardiaDeHospital(); //si no agrego integer no me va a dejar poder implementar infopaciente.nombrePaciente(cola.Primero())
+        GuardiaDeHospital infopaciente = new GuardiaDeHospital();
 
+        cola.InicializarColaPrioridad();
+
+        cola.AcolarPrioridad(20,10);
+        cola.AcolarPrioridad(60,30);
+        cola.AcolarPrioridad(40,20);
+
+
+        System.out.println("Primer turno "+ infopaciente.nombrePaciente(cola.Primero()) + " numero de turno:" + cola.Primero() );
+
+        cola.Desacolar();
+
+        System.out.println("Segundo turno "+ infopaciente.nombrePaciente(cola.Primero()) + " numero de turno:" + cola.Primero() );
+
+        cola.Desacolar();
+
+        System.out.println("Tercer turno "+ infopaciente.nombrePaciente(cola.Primero()) + " numero de turno:" + cola.Primero() );
     }
 }
