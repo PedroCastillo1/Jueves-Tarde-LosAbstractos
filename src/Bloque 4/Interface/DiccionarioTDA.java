15c989105db94f98af75bd2b52b0c5f314b3931f
Mora Camila Coghlan <moracoghlan@gmail.com>
2026-04-15 18:03:00 -0300

    Create DiccionarioTDA.java
    agrego interfaz generica

---
+++ src/Bloque 4/Interface/DiccionarioTDA.java
@@ -0,0 +1,14 @@
+public interface DiccionarioTDA<K, V> {
+
+    void InicializarDiccionario();
+
+    void Agregar(K clave, V valor);
+
+    void Eliminar(K clave);
+
+    boolean Pertenece(K clave);
+
+    V Obtener(K clave);
+
+    boolean DiccionarioVacio();
+}
