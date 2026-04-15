6308508f4f1e244ba9ce17060069f289bbe34bf7
Mora Camila Coghlan <moracoghlan@gmail.com>
2026-04-15 20:26:49 -0300

    Update MainBloque3.java

--- src/Bloque3/MainBloque3.java
+++ src/Bloque3/MainBloque3.java
@@ -1,6 +1,8 @@
 package Bloque3;
 
 import Bloque3.Implementacion.PadronElectoral;
+import Bloque3.Implementacion.ConjuntoMora;
+import Bloque3.Implementacion.Invitado;
 
 public class MainBloque3 {
     public static void main(String[] args) {
@@ -27,7 +29,7 @@ public class MainBloque3 {
         // act 2 (fiesta de fin de año)
 
         
-        Persona p = new Persona(45984446);
+        Invitado i = new Invitado(45984446);
         Integer dni = p.getDni();
         ConjuntoMora c = new ConjuntoMora();
         System.out.println("Ingresa dni " + p.getDni());
@@ -35,12 +37,12 @@ public class MainBloque3 {
         c.InicializarConjunto();
         c.Agregar(dni);
 
-        Persona p2= new Persona(77777777);
+        Invitado i2= new Invitado(77777777);
         Integer dni2 = p2.getDni();
         c.Agregar(dni2);
         System.out.println("Ingresa dni " + p2.getDni());
 
-        Persona p3 = new Persona(55555555);
+        Invitado i3 = new Invitado(55555555);
         Integer dni3 = p3.getDni();
         System.out.println("Ingresa dni " + p3.getDni());
         c.Agregar(dni3);
