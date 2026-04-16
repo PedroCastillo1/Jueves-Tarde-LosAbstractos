ad0ea1e0c0584e60ea7ee7f1541558a29550ded9
Carina Milano <carinamilano@gmail.com>
2026-04-16 10:57:31 -0300

    ..

---
+++ src/Bloque4/Interface/DiccionarioMultipleTDA.java
@@ -0,0 +1,10 @@
+package Bloque4.Interface;
+
+public interface DiccionarioMultipleTDA<K, V> {
+    void InicializarDiccionario();
+    void Agregar(K clave, V valor);
+    void EliminarValor(K clave, V valor);
+    void Eliminar(K clave);
+    V[] Recuperar(K clave);
+    K[] Claves();
+}
